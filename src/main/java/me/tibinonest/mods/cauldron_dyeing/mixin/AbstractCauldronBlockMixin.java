d87d2e2bd0feb64a581e0447413a24b412ec6293
tibs <git@tibinonest.me>
2022-08-18 23:31:19 -0400

    Increase water cauldron block entity compatibility

--- src/main/java/me/tibinonest/mods/cauldron_dyeing/mixin/AbstractCauldronBlockMixin.java
+++ src/main/java/me/tibinonest/mods/cauldron_dyeing/mixin/AbstractCauldronBlockMixin.java
@@ -1,19 +1,24 @@
 package me.tibinonest.mods.cauldron_dyeing.mixin;
 
 import me.tibinonest.mods.cauldron_dyeing.block.WaterCauldronBlockEntity;
+import net.minecraft.block.AbstractCauldronBlock;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(LeveledCauldronBlock.class)
-public class LeveledCauldronBlockMixin implements BlockEntityProvider {
+@Mixin(AbstractCauldronBlock.class)
+public class AbstractCauldronBlockMixin implements BlockEntityProvider {
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return state.getBlock().getLootTableId().getPath().equals("blocks/water_cauldron") ? new WaterCauldronBlockEntity(pos, state) : null;
+        if (state.getBlock() instanceof AbstractCauldronBlock cauldron && cauldron.canBeFilledByDripstone(Fluids.WATER)) {
+            return new WaterCauldronBlockEntity(pos, state);
+        }
+
+        return null;
     }
 }
