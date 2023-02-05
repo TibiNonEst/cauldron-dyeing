db14a2e87a95e6fbebf6fae4c55cea7ad38a95bf
tibs <git@tibinonest.me>
2023-02-05 16:18:15 -0500

    Support Sodium 0.4.9, update deps

--- src/main/java/me/tibinonest/mods/cauldron_dyeing/mixin/sodium/SodiumBlockRendererMixin.java
+++ src/main/java/me/tibinonest/mods/cauldron_dyeing/mixin/sodium/SodiumBlockRendererMixin.java
@@ -4,7 +4,7 @@ import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
 import me.jellysquid.mods.sodium.client.model.quad.blender.ColorBlender;
 import me.jellysquid.mods.sodium.client.model.quad.blender.ColorSampler;
 import me.jellysquid.mods.sodium.client.model.quad.blender.FlatColorBlender;
-import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import me.tibinonest.mods.cauldron_dyeing.block.WaterCauldronBlockEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
@@ -14,13 +14,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockRenderer.class)
 public class SodiumBlockRendererMixin {
-    @Redirect(method = "renderQuad", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/model/quad/blender/ColorBlender;getColors(Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/util/math/BlockPos;Lme/jellysquid/mods/sodium/client/model/quad/ModelQuadView;Lme/jellysquid/mods/sodium/client/model/quad/blender/ColorSampler;Ljava/lang/Object;)[I"))
+    @Redirect(method = "renderQuadList", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/model/quad/blender/ColorBlender;getColors(Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/util/math/BlockPos;Lme/jellysquid/mods/sodium/client/model/quad/ModelQuadView;Lme/jellysquid/mods/sodium/client/model/quad/blender/ColorSampler;Ljava/lang/Object;)[I"))
     private <T> int[] cauldron_dyeing$redirectGetColors(ColorBlender instance, BlockRenderView world, BlockPos pos, ModelQuadView quad, ColorSampler<T> sampler, T state) {
         var blockEntity = world.getBlockEntity(pos);
 
         if (blockEntity instanceof WaterCauldronBlockEntity) {
-            var flatBiomeColorBlender = new FlatColorBlender();
-            return flatBiomeColorBlender.getColors(world, pos, quad, sampler, state);
+            return new FlatColorBlender().getColors(world, pos, quad, sampler, state);
         }
 
         return instance.getColors(world, pos, quad, sampler, state);
